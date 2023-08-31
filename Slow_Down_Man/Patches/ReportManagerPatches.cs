7ad4ce24e99079375d6923f86930a582e66261b1
BiggBenn <37208442+BiggBenn@users.noreply.github.com>
2023-08-31 16:10:20 +0200

    Added ReportManager patch
    Made reported "travel time" etc values adjust to changed day length

---
+++ Slow_Down_Man/Patches/ReportManagerPatches.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlowDownMod
+{
+    public partial class SlowDownMan
+    {
+        /*
+         * ReportManager patch
+         */
+        [HarmonyPatch(typeof(ReportManager))]
+        [HarmonyPatch("ReportValue")]
+        public class Db_Initialize_Patch
+        {
+            public static void Prefix(ReportManager.ReportType reportType, ref float value)
+            {
+                DebugLog("Intercepted ReportManager.ReportValue with type " + reportType + " and value " + value);
+                if(reportType == ReportManager.ReportType.IdleTime 
+                    || reportType == ReportManager.ReportType.TimeSpent 
+                    || reportType == ReportManager.ReportType.TravelTime
+                    || reportType == ReportManager.ReportType.PersonalTime
+                    || reportType == ReportManager.ReportType.WorkTime)
+                {
+                    value /= cycleLengthModifier;
+                }
+            }
+        }
+    }
+}
