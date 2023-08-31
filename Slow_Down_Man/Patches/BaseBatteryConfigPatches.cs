aacaf8031c9ff4a163ed2b5d8a8bb68441cd96f2
BiggBenn <37208442+BiggBenn@users.noreply.github.com>
2023-08-31 18:08:20 +0200

    Set battery drain to be adjusted by the changed cycle length

---
+++ Slow_Down_Man/Patches/BaseBatteryConfigPatches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SlowDownMod
+{
+    public partial class SlowDownMan
+    {
+        /*
+         * BaseBatteryConfig patch
+         */
+        [HarmonyPatch(typeof(BaseBatteryConfig))]
+        [HarmonyPatch("DoPostConfigureComplete")]
+        public class BaseBatteryConfig_DoPostConfigureComplete_Patch
+        {
+            public static void Postfix(GameObject go)
+            {
+                DebugLog("BaseBatteryConfig Postfix");
+                Battery battery = go.AddOrGet<Battery>();
+                battery.joulesLostPerSecond /= cycleLengthModifier;
+            }
+        }
+    }
+}
